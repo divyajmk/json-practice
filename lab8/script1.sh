7188a1af18442d8793a3d5aea21667d5dcdf37ae
divyajmk kuruvilla <yaq7fm@virginia.edu>
2024-04-10 20:33:52 +0000

    started script 1

---
+++ lab8/script1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+
+URL="https://aviationweather.gov/api/data/metar?ids=KMCI&format=json&taf=false&hours=12&bbox=40%2C-90%2C45%2C-85" 
+
+# Retrieve data from NOAA Aviation Weather API
+# Parse data and pull out the receiptTime value 
+# Output only the first six values to the screen 
+RESULT=$(curl URL | jq '.[].receiptTime' | head -n 6)
+
+# Print 
+echo "$data"
