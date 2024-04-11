869373624ac20d4f7bdc14c1352f12a8e35d6903
divyajmk kuruvilla <yaq7fm@virginia.edu>
2024-04-11 01:31:51 +0000

    finished script 2

--- lab8/script2.py
+++ lab8/script2.py
@@ -1,18 +1,26 @@
 #!/home/gitpod/.pyenv/shims/python3
 
+
+
 import json
+
 
 # Read in file as JSON
 with open('schacon.repos.json', 'r') as file:
     data = json.load(file)
 
-# Pull out fields: name, html_url, updated_at, visibility
-# Assembles these four fields into a comma-separated line.
-# Appends this line to a new text file named chacon.csv
+# Create new csv file to append line to later 
+with open("chacon.csv", "w", newline="") as csv_file:
+    # Only want 5 outputs
+    for d in data[:5]:
+        # Pull out fields: name, html_url, updated_at, visibility
+        name = d.get('name')
+        html_url = d.get('html_url')
+        updated_at = d.get('updated_at')
+        visibility = d.get('visibility')
 
-for d in data:
-    print(d['name'])
+        # Assembles these four fields into a comma-separated line.
+        line = name + ',' + html_url + ',' + updated_at + ',' + visibility
 
-
-
-# Limit to 5 lines only
+        # Appends this line to a new text file named chacon.csv
+        csv_file.write(line + '\n')
